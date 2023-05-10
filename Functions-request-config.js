cbe5d09909726b1a9863a02a9c1ffbefb4d6bb23
Muhammadumer.eth <umershaikh217@gmail.com>
2023-05-10 07:51:06 -0700

    open AI function set up

--- Functions-request-config.js
+++ Functions-request-config.js
@@ -31,10 +31,11 @@ const requestConfig = {
   codeLanguage: CodeLanguage.JavaScript,
   // String containing the source code to be executed
   // source: fs.readFileSync("./calculation-example.js").toString(),
-  // source: fs.readFileSync("./Api-request.js").toString(),
-  source: fs.readFileSync("./API-request-example.js").toString(),
+  source: fs.readFileSync("./openAI-Api-request.js").toString(),
+  // source: fs.readFileSync("./API-request-example.js").toString(),
   // Secrets can be accessed within the source code with `secrets.varName` (ie: secrets.apiKey). The secrets object can only contain string values.
-  secrets: { apiKey: process.env.COINMARKETCAP_API_KEY ?? "" },
+  // secrets: { apiKey: process.env.COINMARKETCAP_API_KEY ?? "" },
+  secrets: { openaiKey: process.env.OPEN_AI_API_KEY ?? "" },
   // Per-node secrets objects assigned to each DON member. When using per-node secrets, nodes can only use secrets which they have been assigned.
   perNodeSecrets: [],
   // ETH wallet key used to sign secrets so they cannot be accessed by a 3rd party
@@ -42,7 +43,7 @@ const requestConfig = {
   walletPrivateKey: process.env["PRIVATE_KEY"],
   // Args (string only array) can be accessed within the source code with `args[index]` (ie: args[0]).
   // args: ["1", "bitcoin", "btc-bitcoin", "btc", "1000000", "450"],
-  // args: ["write ruleset for content moderation", "", "", "", "", ""],
+  args: ["I am going to kill you ", "", "", "", "", ""],
   // Expected type of the returned value
   expectedReturnType: ReturnType.uint256,
   // Redundant URLs which point to encrypted off-chain secrets
--- openAI-Api-request.js
+++ openAI-Api-request.js
@@ -2,8 +2,6 @@ const prompt = args[0]
 // const Rules = args[1]
 // const content = args[2]
 
-const OPEN_AI_API_KEY = process.env.OPEN_AI_API_KEY
-
 if (!secrets.openaiKey) {
   throw Error("Need to set OPENAI_KEY environment variable")
 }
@@ -13,17 +11,35 @@ if (!secrets.openaiKey) {
 
 // example response:
 // {"id":"cmpl-6jFdLbY08kJobPRfCZL4SVzQ6eidJ","object":"text_completion","created":1676242875,"model":"text-davinci-003","choices":[{"text":"\n\nThis is indeed a test","index":0,"logprobs":null,"finish_reason":"length"}],"usage":{"prompt_tokens":5,"completion_tokens":7,"total_tokens":12}}
+// const openAIRequest = Functions.makeHttpRequest({
+//   url: "https://api.openai.com/v1/completions",
+//   method: "POST",
+//   headers: {
+//     Authorization: `Bearer ${secrets.openaiKey}`,
+//   },
+//   data: { model: "text-davinci-003", prompt: prompt, temperature: 0, max_tokens: 25 },
+// })
+
 const openAIRequest = Functions.makeHttpRequest({
-  url: "https://api.openai.com/v1/completions",
+  url: "https://api.openai.com/v1/moderations",
   method: "POST",
   headers: {
     Authorization: `Bearer ${secrets.openaiKey}`,
   },
-  data: { model: "text-davinci-003", prompt: prompt, temperature: 0, max_tokens: 7 },
+  data: {
+    model: "content-filter-alpha-1",
+    prompt: `Please moderate this text: "${prompt}"`,
+    temperature: 0,
+    max_tokens: 3,
+    labels: ["Safe", "Potentially harmful", "Unsafe"],
+  },
 })
 
 const [openAiResponse] = await Promise.all([openAIRequest])
 console.log("raw response", openAiResponse)
 
+onsole.log("Result =", openAiResponse.data.choices[0].text)
+onsole.log("Result =", openAiResponse.data.choices[0].text)
 const result = openAiResponse.data.choices[0].text
+
 return Functions.encodeString(result)
